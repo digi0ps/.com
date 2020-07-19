b11fedc8ffb0b91f77bb3d1f4a09ce3b1a992296
digi0ps <sriru1998@gmail.com>
2020-07-20 00:26:19 +0530

    Add routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-import "./App.css";
+import { Router } from "@reach/router";
+
+// Components
+import Home from "./components/home";
+import NotFound from "./components/not-found";
+import About from "./components/about";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Home path="/" exact />
+      <About path="/whoami" exact />
+      <NotFound default />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/not-found.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NotFound() {
+  return <div>Ooops Not Found</div>;
+}
+
+export default NotFound;
