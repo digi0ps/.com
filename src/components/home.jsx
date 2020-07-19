8ddfb3642c30e18ea0c4eb401b978f7a96c3bd07
digi0ps <sriru1998@gmail.com>
2020-07-20 00:47:08 +0530

    Add homepage with basic styles.

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Link } from "@reach/router";
+
+import "./home.css";
+
+const LINKS = [
+  ["ls /blog", "/blog"],
+  ["ls /projects", "/projects"],
+  ["cat contact.md", "/contact"],
+  ["whoami", "/whoami"],
+];
 
 function Home() {
-  return <div>digi0ps</div>;
+  const toggleTheme = (e) => {
+    e.preventDefault();
+    document.body.classList.toggle("dark");
+  };
+
+  return (
+    <div className="home">
+      <header>
+        <h1 className="home-title">
+          <span onClick={toggleTheme} className="home-icon">
+            🏕
+          </span>{" "}
+          digi0ps
+        </h1>
+      </header>
+      <section>
+        {LINKS.map(([text, url]) => (
+          <div className="home-link">
+            <Link to={url}>
+              {">"} {text}
+            </Link>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
 }
 
 export default Home;
